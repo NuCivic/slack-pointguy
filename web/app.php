02564b9e2e3b812ae4e227374c2fd7a39e9dd267
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-09-01 14:17:04 -0300

    + Sets token and sheet as environment variables + Removes config file

--- web/app.php
+++ web/app.php
@@ -1,7 +1,6 @@
 <?php
 
 require('../vendor/autoload.php');
-require_once('config.php');
 require_once('functions.php');
 
 use Symfony\Component\HttpFoundation\Request;
@@ -9,42 +8,51 @@ use Symfony\Component\HttpFoundation\Request;
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+$config = array(
+  'token' => getenv("TOKEN"),
+  'sheet' => getenv("SHEET"),
+);
 
-$app->get('/', function() use($app) {
-  return $app->json(
-    array(
-      'reason' => 'no get',
-      'text' => 'i don\'t talk get with strangers',
-    ),
-    403
-  );
-});
+if ($config['token'] && $config['sheet']) {
+  // Register the monolog logging service
+  $app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+  ));
 
-$app->post('/', function(Request $request) use($app, $config) {
-  if ($request->get('token') == $config['token']) {
-    $app['monolog']->addDebug('token is correct.');
-    $sheet = load_payroll($config['sheet']);
-    $text = $request->get('text');
-    $result = search_pointperson($text, $sheet);
-    $response_text = sprintf('The point-person for %s is %s %s', $text, $result['name'], $result['slack'] );
-    $response = array(
-      'respose_type' => 'in_channel',
-      'text' => $response_text
+  $app->get('/', function() use($app) {
+    return $app->json(
+      array(
+        'reason' => 'no get',
+        'text' => 'i don\'t talk get with strangers',
+      ),
+      403
     );
-    return $app->json($response, 200);
-  }
-  $app['monolog']->addDebug('incorrect token');
-  return $app->json(
-    array(
-      'reason' => 'incorrect token',
-      'text' => 'I don\'t talk post to strangers'
-    ),
-    403
-  );
-});
+  });
 
+  $app->post('/', function(Request $request) use($app, $config) {
+    if ($request->get('token') == $config['token']) {
+      $app['monolog']->addDebug('token is correct.');
+      $sheet = load_payroll($config['sheet']);
+      $text = $request->get('text');
+      $result = search_pointperson($text, $sheet);
+      $response_text = sprintf('The point-person for %s is %s %s', $text, $result['name'], $result['slack'] );
+      $response = array(
+        'respose_type' => 'in_channel',
+        'text' => $response_text
+      );
+      return $app->json($response, 200);
+    }
+    $app['monolog']->addDebug('incorrect token');
+    return $app->json(
+      array(
+        'reason' => 'incorrect token',
+        'text' => 'I don\'t talk post to strangers'
+      ),
+      403
+    );
+  });
 $app->run();
+}
+else {
+  echo "Please set TOKEN and SHEET environment variables";
+}
