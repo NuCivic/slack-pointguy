bc33ed8cc59fd00712d844dd4e3541ba9ed4ca39
Teofilo Sibileau <teo.sibileau@gmail.com>
2016-09-01 14:17:04 -0300

    + Sets token and sheet as environment variables + Removes config file

--- web/config.demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config = array(
-  'token' => 'XEDLdo7T3QqYNsbZ38UInqUh',
-  'sheet' => 'https://docs.google.com/spreadsheet/pub?key=fd-vD1v-FuXvMB-6laF4F_m5Gsr7fhsDM&single=true&gid=0&output=csv',
-);
